d42a7043d9fed3bd92984a838c6be495c9d8c3cb
Charles Guan <cguan@caltech.edu>
2022-10-20 10:42:38 -0700

    Add MDS plot to plot_rdms.py

--- scripts/plot_rdms.py
+++ scripts/plot_rdms.py
@@ -2,10 +2,17 @@
 
 import argparse
 import pathlib
+from typing import List, Optional
 
 import matplotlib as mpl
+import matplotlib.pyplot as plt
 import numpy as np
+import prince
 import rsatoolbox
+import sklearn.manifold
+import xarray as xr
+
+from fingers_rsa import mds_utils
 
 
 def main() -> None:
@@ -24,7 +31,9 @@ def main() -> None:
         vmin=0,
         nanmask=np.zeros((rdms.n_cond, rdms.n_cond), dtype=bool),
     )
-    fig.savefig(args.output_dir.joinpath(f"rdm_all_{args.rdm_descriptor}"))
+    save_path = args.output_dir.joinpath(f"rdm_all_{args.rdm_descriptor}")
+    print("Saving RDM plot to:", save_path)
+    fig.savefig(save_path)
 
     # Plot average RDM
     fig, _, _ = rsatoolbox.vis.show_rdm(
@@ -35,7 +44,15 @@ def main() -> None:
         vmin=0,
         nanmask=np.zeros((rdms.n_cond, rdms.n_cond), dtype=bool),
     )
-    fig.savefig(args.output_dir.joinpath("rdm_mean"))
+    save_path = args.output_dir.joinpath("rdm_mean")
+    print("Saving mean RDM plot to:", save_path)
+    fig.savefig(save_path)
+
+    fig, ax = plt.subplots()
+    plot_mds(rdms, colors=args.colors, ax=ax)
+    save_path = args.output_dir.joinpath("mds")
+    print("Saving MDS plot to:", save_path)
+    fig.savefig(save_path)
 
 
 def parse_args() -> argparse.Namespace:
@@ -65,8 +82,52 @@ def parse_args() -> argparse.Namespace:
         default="session",
         help="Which RDM descriptor to label RDM axes with",
     )
+    parser.add_argument(
+        "--colors",
+        "-c",
+        default=["#4477AA", "#66CCEE", "#228833", "#CCBB44", "#EE6677"],
+        help="Order of colors to use for pattern-descriptor plotting",
+    )
 
     return parser.parse_args()
+
+
+def plot_mds(
+    rdms: rsatoolbox.rdm.RDMs,
+    rdm_descriptor: str = "session",
+    pattern_descriptor: str = "finger",
+    colors: Optional[List[str]] = None,
+    scale: bool = True,
+    ax: mpl.axes.Axes = None,
+) -> None:
+    """Plot multidimensional scaling of representational dissimilarities."""
+    # Pre-allocate array for MDS coordinates
+    mds_da = xr.DataArray(
+        np.nan,
+        dims=[rdm_descriptor, pattern_descriptor, "mds_dim"],
+        coords={
+            rdm_descriptor: rdms.rdm_descriptors[rdm_descriptor],
+            pattern_descriptor: rdms.pattern_descriptors[pattern_descriptor],
+            "mds_dim": ["mds_1", "mds_2"],
+        },
+        name="mds",
+    )
+    rdm_mat = rdms.get_matrices()
+    mds = sklearn.manifold.MDS(dissimilarity="precomputed")
+    for idx in range(rdms.n_rdm):
+        mds_da[{rdm_descriptor: idx}] = mds.fit_transform(rdm_mat[idx])
+    assert not mds_da.isnull().any(), "MDS returned NaN for some RDMs"
+
+    # Align MDS coordinates
+    mds_da.values = prince.GPA(scale=scale).fit_transform(mds_da.values)
+
+    # Assign some colors to the condition-dimension
+    mds_utils.make_mds_plot_prealigned(
+        mds_da.values,
+        ax=ax,
+        condition_order=mds_da.coords[pattern_descriptor].values,
+        colors=colors,
+    )
 
 
 if __name__ == "__main__":
